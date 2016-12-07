f57ba2b861bf72b3a102db86cc0005ce79a717b7
Kalimaha <guido.barbaglia@gmail.com>
2016-12-07 22:43:11 +1100

    First implementation.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Kalimaha.github.io
-My personal blog.
+# Dependencies
+
+* [nodejs](https://nodejs.org/en/) installed
+* [grunt-cli](http://gruntjs.com/getting-started) installed
+
+# Usage
+
+Install all the dependencies with:
+
+```
+npm install
+```
+
+Create the blog posts with:
+
+```
+grunt
+```
+
+# How to add an article
+
+Add a new `.md` file in the `src/contents` folder, e.g. `my_post.md`.
+The name must be in snake case, capitalize the words as required.
+
+To add a summary to the post, create a new `.md` file in `src/summaries` with
+the same name of the post file (_all small caps this time_).
+
+To add an image to the post, add a new `.png` file in `src/images` with the
+same name of the post (_all small caps again_).
+
+The HTML files will be stored in the `posts` folder.
--- src/tasks/create_posts.js
+++ src/tasks/create_posts.js
@@ -1,27 +1,65 @@
+/*global module, require*/
 (function () {
 
   'use strict';
 
   const get_filenames = (grunt) => {
-    var filenames = []
+    var filenames = [];
 
     grunt.file.recurse('./src/contents', function(a, b, c, filename) {
       filenames.push(filename)
-    })
+    });
 
     return filenames
-  }
+  };
+
+  const create_post_title = (filename) => {
+    const title = filename.substring(0, filename.indexOf('.md')).replace(/_/g, " ");
+
+    return `${title.charAt(0).toUpperCase()}${title.slice(1)}`
+  };
+
+  const create_html_name = (filename) => {
+    return `${filename.substring(0, filename.indexOf('.md'))}.html`.toLocaleLowerCase()
+  };
+
+  const create_logo = (filename) => {
+    return `${filename.substring(0, filename.indexOf('.md'))}.png`.toLocaleLowerCase()
+  };
+
+  const create_post = (grunt, filename) => {
+    const showdown          = require('showdown');
+    const converter         = new showdown.Converter({'tables': true});
+    const markdown_content  = grunt.file.read(`./src/contents/${filename}`, [null, {encoding: 'utf8'}]);
+    const html_content      = converter.makeHtml(markdown_content);
+
+    const Handlebars        = require('handlebars');
+    const hbs               = grunt.file.read('./src/templates/post.hbs', [null, {encoding: 'utf8'}]);
+    const template          = Handlebars.compile(hbs);
+
+    const summary_markdown  = grunt.file.read(`./src/summaries/${filename}`, [null, {encoding: 'utf8'}]);
+    const summary_content   = converter.makeHtml(summary_markdown);
+
+    const html = template({
+      title: create_post_title(filename),
+      description: summary_content,
+      html_name: create_html_name(filename),
+      logo: create_logo(filename),
+      date_published: new Date().toISOString(),
+      date_modified: new Date().toISOString(),
+      content: html_content,
+      url: `http://guido-barbaglia.blog/posts/${create_html_name(filename)}`
+    });
+
+    grunt.file.write(`./posts/${create_html_name(filename)}`, html, [null, {encoding: 'utf8'}]);
+  };
 
   module.exports = function (grunt) {
-
-
     grunt.registerTask('create_posts', 'Create HTML posts.', function () {
-      const filenames = get_filenames(grunt)
-      console.log(filenames)
+      const filenames = get_filenames(grunt);
+      for (var filename of filenames) {
+        create_post(grunt, filename)
+      }
     })
   }
-
-
-
-
-}())
+}());
